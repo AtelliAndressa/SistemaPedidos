9bb7b9c7f170b57d97ddea46c3cc5a0e899687f7
Andressa Atelli <andressaatelli.programador@gmail.com>
2022-07-19 18:12:15 -0300

    formação do domain

---
+++ SistemaPedidos/Domain/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidos.Domain
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Cidade { get; set; }
+    }
+}
---
+++ SistemaPedidos/Domain/Produto.cs
@@ -0,0 +1,25 @@
+﻿using SistemaPedidos.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidos.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string CodigoBarras { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public TipoProduto TipoProduto { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
