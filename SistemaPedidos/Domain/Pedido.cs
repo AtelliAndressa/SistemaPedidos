b242ee95c53fea2bbd517a5ed4ee12a9c940ced6
Andressa Atelli <andressaatelli.programador@gmail.com>
2022-07-19 18:37:28 -0300

    entidade do pedido

---
+++ SistemaPedidos/Domain/Pedido.cs
@@ -0,0 +1,30 @@
+﻿using SistemaPedidos.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidos.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public DateTime IniciadoEm { get; set; }
+
+        public DateTime FinalizadoEm { get; set; }
+
+        public TipoFrete TipoFrete { get; set; }
+
+        public StatusPedido Status { get; set; }
+
+        public string Observacao { get; set; }
+
+        public ICollection<PedidoItem> Item { get; set; }
+    }
+}
---
+++ SistemaPedidos/Domain/PedidoItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidos.Domain
+{
+    public class PedidoItem
+    {
+        public int Id { get; set; }
+
+        public int PedidoId { get; set; }
+
+        /// <summary>
+        /// Propriedade de navegação Pedido
+        /// </summary>
+        public Pedido Pedido { get; set; }
+
+        public int ProdutoId { get; set; }
+
+        /// <summary>
+        /// Propriedade de navegação Produto
+        /// </summary>
+        public Produto Produto { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public decimal ValorDesconto { get; set; }
+
+    }
+}
